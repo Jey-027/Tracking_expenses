493c371fcd62a359f224e2dc795aa2c9cc23d6e1
jey-027 <jdrodriguezg25@hotmail.com>
2022-12-31 21:21:48 -0500

    add url file

--- Tracking_expenses_web/tracking_app/urls.py
+++ Tracking_expenses_web/tracking_app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="Home"),
     path('category/list', views.CategoryList.as_view(), name="Categories"),
+    path('entity/list', views.EntityList.as_view(), name="Entities"),
+    path('bank_product/list', views.BankProductList.as_view(), name="Bank Products"),
+    path('payment_method/list', views.PaymentMethodList.as_view(), name="PaymentMethods"),
 ]
--- Tracking_expenses_web/tracking_app/views.py
+++ Tracking_expenses_web/tracking_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Category
+from .models import Category, Entity, BankProduct, PaymentMethod
 
 
 # Create your views here.
@@ -13,3 +13,21 @@ class CategoryList(ListView):
     model = Category
     context_object_name = "category_list"
     template_name = "tracking_app/list_category.html"
+
+
+class EntityList(ListView):
+    model = Entity
+    context_object_name = "entity_list"
+    template_name = "tracking_app/list_entity.html"
+
+
+class BankProductList(ListView):
+    model = BankProduct
+    context_object_name = "bank_product_list"
+    template_name = "tracking_app/list_bankProduct.html"
+
+
+class PaymentMethodList(ListView):
+    model = PaymentMethod
+    context_object_name = "payment_method_list"
+    template_name = "tracking_app/list_paymentMethod.html"
