872d7582ae7c51ecc13ef41c4d53f64b2ecd720b
jey-027 <jdrodriguezg25@hotmail.com>
2023-01-03 16:49:20 -0500

    add template create and views create

--- Tracking_expenses_web/tracking_app/urls.py
+++ Tracking_expenses_web/tracking_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.home, name="Home"),
     path('category/list', views.CategoryList.as_view(), name="Categories"),
     path('entity/list', views.EntityList.as_view(), name="Entities"),
-    path('bank_product/list', views.BankProductList.as_view(), name="Bank Products"),
+    path('bank_product/list', views.BankProductList.as_view(), name="Bank_Products"),
     path('payment_method/list', views.PaymentMethodList.as_view(), name="PaymentMethods"),
+    path('category/create', views.CreateCategory.as_view(), name="Create_categories"),
+    path('entity/create', views.CreateEntity.as_view(), name="Create_entity"),
+    path('bank_product/create', views.CreateBankProduct.as_view(), name="Create_bankProduct"),
+    path('payment_method/create', views.CreatePaymentMethod.as_view(), name="Create_paymentMethod"),
 ]
--- Tracking_expenses_web/tracking_app/views.py
+++ Tracking_expenses_web/tracking_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Category, Entity, BankProduct, PaymentMethod
 
 
@@ -15,10 +16,30 @@ class CategoryList(ListView):
     template_name = "tracking_app/list_category.html"
 
 
+class CreateCategory(CreateView):
+    model = Category
+    template_name = "tracking_app/create_category.html"
+    fields = ('category_name',)
+    success_url = "/category/list"
+
+
+class UpdateCategory(UpdateView):
+    model = Category
+    template_name = ""
+    fields = ('category_name',)
+
+
 class EntityList(ListView):
     model = Entity
     context_object_name = "entity_list"
     template_name = "tracking_app/list_entity.html"
+
+
+class CreateEntity(CreateView):
+    model = Entity
+    template_name = "tracking_app/create_entities.html"
+    fields = ('entity_name',)
+    success_url = "/entity/list"
 
 
 class BankProductList(ListView):
@@ -27,7 +48,21 @@ class BankProductList(ListView):
     template_name = "tracking_app/list_bankProduct.html"
 
 
+class CreateBankProduct(CreateView):
+    model = BankProduct
+    template_name = "tracking_app/create_bank_product.html"
+    fields = ('product_name', 'id_entity',)
+    success_url = "/bank_product/list"
+
+
 class PaymentMethodList(ListView):
     model = PaymentMethod
     context_object_name = "payment_method_list"
     template_name = "tracking_app/list_paymentMethod.html"
+
+
+class CreatePaymentMethod(CreateView):
+    model = PaymentMethod
+    template_name = "tracking_app/create_payment_method.html"
+    fields = ('method_name',)
+    success_url = "/payment_method/list"
